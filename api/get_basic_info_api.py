d2bb89068e380fb1523219ebaca2835e4ba694ef
HaOooMi <2337609402@qq.com>
2025-08-11 23:52:24 +0800

    修改utils

--- api/get_basic_info_api.py
+++ api/get_basic_info_api.py
@@ -4,7 +4,7 @@ from typing import Optional
 from datetime import date
 from sqlalchemy.engine import Connection
 
-from stock_info import utils
+from utils import get_mysql_engine
 import sys
 sys.path.append('d:/vscode projects/stock')
 from stock_info.stock_meta_akshare import get_basic_info_mysql
@@ -27,7 +27,7 @@ basic=APIRouter()
 @basic.get("/basical-info/{stock_code}", response_model=Optional[StockBasicInfo], tags=["股票基本数据"])
 def get_basic_info(
     stock_code: str= Path(..., description="单个股票代码", example="000001"), 
-    db: Connection = Depends(utils.get_mysql_engine)):
+    db: Connection = Depends(get_mysql_engine)):
     raw_data = get_basic_info_mysql(db, (stock_code,))
     if stock_code in raw_data:
         return StockBasicInfo(**raw_data[stock_code])
